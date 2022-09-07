ce4d7a4a8a5f7566dae39b31680b656fe02d26b9
Pacifist <desmondmorales92@gmail.com>
2022-09-07 19:15:37 +0300

    deleting garbage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Modal from "./components/Modal";
 import Component from "./components/Component";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
